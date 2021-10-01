f8c565424fc1a455962dce80de8521b4665d0053
Tetiana-Soliarova <t.s.skorpi1991@ukr.net>
2021-10-01 20:38:27 +0300

    чистка кода

--- js/map.js
+++
@@ -1,8 +0,0 @@
-function myMap() {
-    const mapCanvas = document.getElementById('map');
-    const mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2),
-        zoom: 10
-    };
-    const map = new google.maps.Map(mapCanvas, mapOptions);
-}
